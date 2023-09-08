a26d78c708b3a9da4d71e84c5a292d201aaf3d59
Helam7u7 <helam053@gmail.com>
2023-09-08 13:19:28 -0500

    img product  list

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Category.css'
 
 const Category = () => {
   return (
@@ -6,8 +7,20 @@ const Category = () => {
       <div className='list_product'>
         <h2>IPhone X </h2>
         <div className='container_products'>
-          <div className='product_img'><img src="/img/iphone/fono-1-(IPhone-14-pro-Max-dorrado).png" alt="" /></div>
-          <div className='product_img'><img src="/img/iphone/Fono2(IPhone-14-pro-Max-morado).png" alt="" /></div>
+          <div className='product_card'>
+            <div className='card_img'><img src="/img/iphone/fono-1-(IPhone-14-pro-Max-dorrado).png" alt="" /></div>
+            <div className='card_info'>
+              <h3>IPhone 14 Pro Max</h3>
+              <h2>S/. 6,049.00</h2>
+            </div>
+          </div>
+          <div className='product_card'>
+            <div className='card_img'><img src="/img/iphone/Fono2(IPhone-14-pro-Max-morado).png" alt="" /></div>
+            <div className='card_info'>
+              <h3>IPhone 14 Pro Max</h3>
+              <h2>S/. 6,049.00</h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
