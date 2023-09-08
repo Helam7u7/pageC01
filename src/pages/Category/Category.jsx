0da00f2a80e561951910bd26286cf50e73ac183f
Helam7u7 <helam053@gmail.com>
2023-09-08 13:43:57 -0500

    v.2

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -5,7 +5,7 @@ const Category = () => {
   return (
     <div className='container_categories'>
       <div className='list_product'>
-        <h2>IPhone X </h2>
+        <h2>Generaciones: PIPIPI </h2>
         <div className='container_products'>
           <div className='product_card'>
             <div className='card_img'><img src="/img/iphone/fono-1-(IPhone-14-pro-Max-dorrado).png" alt="" /></div>
