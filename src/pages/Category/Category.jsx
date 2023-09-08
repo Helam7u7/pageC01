dfed29797ae5cb4106ab94855bee620ed6578088
Helam7u7 <helam053@gmail.com>
2023-09-08 12:42:57 -0500

    xd

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <div className='container_categories'>
+      <div className='list_product'>
+        <h2>IPhone X </h2>
+        <div className='container_products'>
+          <div className='product_img'><img src="/img/iphone/fono-1-(IPhone-14-pro-Max-dorrado).png" alt="" /></div>
+          <div className='product_img'><img src="/img/iphone/Fono2(IPhone-14-pro-Max-morado).png" alt="" /></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
