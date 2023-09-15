f3162d40cdf4093841e096607035545a5eb2a66f
Helam7u7 <helam053@gmail.com>
2023-09-15 11:28:41 -0500

    carrusel

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -3,36 +3,45 @@ import './Carrusel.css';
 
 const Carrusel = ({ productoObj }) => {
   const listProducts = productoObj.productos;
-  const [currentIndex, setCurrentIndex] = useState(0); // Initialize with 0
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const goToPreviousSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? listProducts.length - 1 : prevIndex - 1));
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? listProducts.length - 2 : prevIndex - 2
+    );
   };
 
-  const goToNextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === listProducts.length - 1 ? 0 : prevIndex + 1));
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === listProducts.length - 2 ? 0 : prevIndex + 2
+    );
   };
-  console.log(listProducts);
+
   return (
-    <>
-      <h2>Generaciones:</h2>
-    <div className="carrusel">
-      {listProducts.map(produt => (
-        <img src={produt.img} alt="" />
-      ))}
-      {/* <button className="arrow-button left" onClick={goToPreviousSlide}>
-        &lt;
-      </button> */}
-      <div className='carrusel_img'>
-      {/* {listProducts.length > 0 ?  */}
-        {/* <img src={listProducts[currentIndex].img} alt={`Image ${listProducts[currentIndex].modelo}`} /> : null } */}
-        {/* <h3>{listProducts[currentIndex].modelo}</h3> */}
+    <div className="carrusel-container">
+      <button className="prev-button" onClick={prevSlide}>
+        &#8249;
+      </button>
+      <button className="next-button" onClick={nextSlide}>
+        &#8250;
+      </button>
+      <div className="carrusel">
+        {listProducts.map((product, index) => (
+          <div
+            key={product.id}
+            className={`carrusel-slide ${
+              index === currentIndex || index === currentIndex + 1 ? 'active' : ''
+            }`}
+          >
+            <img src={product.img} alt={product.modelo} />
+            <h3>{product.modelo}</h3>
+            <p>{product.color}</p>
+            <p>{product.almacenamiento}</p>
+            <p>${product.precio}</p>
+          </div>
+        ))}
       </div>
-      {/* <button className="arrow-button right" onClick={goToNextSlide}> */}
-        {/* &gt; */}
-      {/* </button> */}
     </div>
-        </>
   );
 };
 
