1d535933b6e6e1dc85c3fdf76d5cdfe3ac7004a5
Helam7u7 <helam053@gmail.com>
2023-09-23 14:04:01 -0500

    jj

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
 
 const Layout = ({categories_data}) => {
   return (
     <>
       <Navbar categories_data = {categories_data}/>
       <Outlet/>
+      <Footer/>
     </>
   )
 }
