f0f0498057e05117d03e8fb5247b82a29bdeafcb
Helam7u7 <helam053@gmail.com>
2023-09-24 12:37:01 -0500

    burguer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React,{useState}from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = ({categories_data}) => {
   const categories = categories_data.categorias
   
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+
   return (
-    <nav className='container_navbar'>
+    <nav className={`container_navbar ${menuOpen ? 'open' : ''}`}>
       <div className='navbar_img'>
         <Link to='/'><img src="/img/LogoCliente.png" alt="" /></Link>
       </div>
-      <div className='navbar_list'>
+      <div className={`navbar_list ${menuOpen ? 'open' : ''}`}>
         <ul className='list_links'>
         {categories.map(categoria => (
             <li key={categoria.id}><Link className='list_link' to={`/categoria/${categoria.categoria}`}>{categoria.categoria}</Link></li>
           ))}
         </ul>
       </div>
+      <div className='navbar_burguer' onClick={toggleMenu}><img src="/img/icons/burguer.png" alt="" /></div>
     </nav>
   )
 }
