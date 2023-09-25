5417d7009ac0dc1bcaeec978047be66c32e391bf
Helam7u7 <helam053@gmail.com>
2023-09-25 12:59:39 -0500

    responsive8

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <div className='container_banner'>
-        <img src="/img/banner/portada1.png" alt="" />
+        <img src="/img/banner/portada2.png" alt="" />
       </div>
     </>
   )
