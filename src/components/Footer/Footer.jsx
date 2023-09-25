6fb0a8e79ffb3f2b97598cff1ba7c83b6ef5ebd5
Helam7u7 <helam053@gmail.com>
2023-09-25 14:06:14 -0500

    df

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
     <div className='footer'>
       <div className='footer_cont servicios'>
         <h2>Productos</h2>
-        {/* <h3>Ofrecemos venta de productos Apple como:</h3> */}
         <ul>
           <li>📱 Iphone's</li>
           <li>💻 Mac</li>
           <li>🎧 Airpods</li>
           <li>🔋 Cargadores</li>
         </ul>
+        <h3>"Ofrecemos venta de productos Apple"</h3>
       </div>
       <div className='footer_cont contacto'>
         <h2>Contáctanos</h2>
@@ -20,13 +20,19 @@ const Footer = () => {
           <div className='num_img'><img src="/img/icons/whatsapp.png" alt="" /></div>
           <h3 className='num_txt'>+51 995894477</h3>
         </div>
-        <div className='contacto_num'>
+          <div className='contacto_num'>
           <div className='num_img'><img src="/img/icons/whatsapp.png" alt="" /></div>
           <h3 className='num_txt'>+51 962848631</h3>
         </div>
+        <div className='qr_img'>
+          <img src="/img/icons/qr/qr1.jpeg" alt="" />
+          <img src="/img/icons/qr/qr2.jpeg" alt="" />
+        </div>
+      </div>
+        <div>
       </div>
       <div className='footer_cont eslogan'>
-        <p className='eslogan_txt'>¡Porque ser genial nunca pasas de moda!</p>
+        <h2 className='eslogan_txt'>¡Porque ser genial nunca pasas de moda!</h2>
         <div className='eslogan_img'><img src="/img/logo del cliente 1.png" alt="" /></div>
       </div>
     </div>
