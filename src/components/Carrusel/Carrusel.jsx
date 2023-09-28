e0e0eae69a97dc4d39bc343572eec941fbaf0bc3
Helam7u7 <helam053@gmail.com>
2023-09-28 10:34:00 -0500

    responsive33

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,34 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carrusel.css';
 import { Link } from 'react-router-dom';
 
 const Carrusel = ({ productoObj, categoria }) => {
   const listProducts = productoObj.productos;
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
 
   const duplicatedProducts = [...listProducts, ...listProducts]; // Duplica los productos
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? duplicatedProducts.length - 2 : prevIndex - 2));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? duplicatedProducts.length - (isMobile ? 1 : 2) : prevIndex - 1
+    );
   };
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === duplicatedProducts.length - 2 ? 0 : prevIndex + 2));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === duplicatedProducts.length - (isMobile ? 1 : 2) ? 0 : prevIndex + 1
+    );
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 500);
+    };
+
+    // Agregar un listener para detectar cambios en el tamaño de la ventana
+    window.addEventListener('resize', handleResize);
+
+    // Llamar a handleResize inicialmente para determinar el estado móvil
+    handleResize();
+
+    // Eliminar el listener al desmontar el componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
       <div className='bannerGen'><img src="/img/banner/xdxd.png" alt="" /></div>
-      
-      
-      <div className="carrusel">
+
+      <div className={`carrusel${isMobile ? ' mobile' : ''}`}>
         <button className="carrusel-btn prev" onClick={prevSlide}>
           &lt;
         </button>
         <div className="carrusel-container">
-          {duplicatedProducts.slice(currentIndex, currentIndex + 2).map((producto, index) => (
+          {duplicatedProducts.slice(currentIndex, currentIndex + (isMobile ? 1 : 2)).map((producto, index) => (
             <div key={index} className="carrusel-item">
-              <Link to={`/categoria/${categoria}/${producto.id}`}><img src={producto.imgMain} alt={producto.modelo} /></Link>
+              <Link to={`/categoria/${categoria}/${producto.id}`}>
+                <img src={producto.imgMain} alt={producto.modelo} />
+              </Link>
               <p>{producto.modelo} {producto.almacenamiento}</p>
               <p className='precio'>S/.{producto.precio}</p>
             </div>
